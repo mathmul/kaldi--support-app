4085dd91b48c2321fa4cd0a143259bb5458d9a5f
mathmul <mmulej@gmail.com>
2025-07-26 02:35:27 +0200

    Auto seed Users only in %dev profile

--- com.kaldi.support/src/main/java/com/kaldi/support/UserSeeder.java
+++ com.kaldi.support/src/main/java/com/kaldi/support/UserSeeder.java
@@ -2,6 +2,7 @@ package com.kaldi.support;
 
 import com.kaldi.support.model.User;
 import com.kaldi.support.model.UserRole;
+import io.quarkus.arc.profile.IfBuildProfile;
 import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.runtime.Startup;
 import jakarta.annotation.PostConstruct;
@@ -10,6 +11,7 @@ import jakarta.transaction.Transactional;
 
 
 @Startup
+@IfBuildProfile("dev")
 @ApplicationScoped
 public class UserSeeder {
 
@@ -27,12 +29,16 @@ public class UserSeeder {
             u.username = "alice";
             u.password = BcryptUtil.bcryptHash("secret");
             u.persist();
+            System.out.println("Added user 'alice' with password 'secret' and role 'USER' to database.");
 
             User o = new User();
             o.setRole(UserRole.OPERATOR);
             o.username = "admin";
             o.password = BcryptUtil.bcryptHash("admin123");
             o.persist();
+            System.out.println("Added user 'admin' with password 'admin123' and role 'OPERATOR' to database.");
+        } else {
+            System.out.println("Users already exist.");
         }
     }
 }
